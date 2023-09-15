468921c7d007c973df7950af96031c92b263aa98
SugarCube1337 <126420288+SugarCube1337@users.noreply.github.com>
2023-09-15 16:48:36 +0300

    decomposed terrible things

--- Client/src/main/java/org/lab6/task/ExecuteTask.java
+++ Client/src/main/java/org/lab6/task/ExecuteTask.java
@@ -24,59 +24,94 @@ public class ExecuteTask implements Task {
             System.out.println("You need to specify the file name, the usage: execute_script [file_name]");
             return;
         }
-        List<String> commands = new ArrayList<>();// List to store the commands from the script
-        StringBuilder line = new StringBuilder();
-        executedScripts.add(args[0]);// Add the current script to the executedScripts list
+
+        List<String> commands = new ArrayList<>();
+        executedScripts.add(args[0]);
+
         try {
             InputStreamReader reader = new InputStreamReader(new FileInputStream(args[0]));
-            int c;
-            // Read the script file character by character
-            while ((c = reader.read()) != -1) {
-                if ((char) c == '\n') {
-                    String finished = line.toString();
-                    line = new StringBuilder();
-                    if (finished.startsWith("execute_script")) {// Check if the line starts with "execute_script" (recursion)
-                        boolean pass = true;
-                        // Check if the script has already been executed to prevent recursion
-                        for (String script : new ArrayList<>(executedScripts))
-                            if (finished.contains(script)) {
-                                System.out.println("You cannot run a script that has been run before");
-                                pass = false;
-                                break;
-                            }
-                        // If not executed before, add it to the list of commands
-                        if (pass) {
-                            executedScripts.add(finished.replace("\r", ""));
-                            commands.add(finished.replace("\r", "")); // Windows adaptation
-                        }
-                    } else
-                        // If it's not an "execute_script" command, add it to the list of commands
-                        commands.add(finished.replace("\r", "")); // Windows adaptation
-                } else
-                    // Append characters to the current line
-                    line.append((char) c);
-            }
+            parseScriptFile(reader, commands);
         } catch (IOException ex) {
             System.out.println("Failed to read data from file");
         }
-        if (line.length() > 0 && line.toString().replace("\r", "").replace("\n", "").length() > 0) {
-            if (line.toString().startsWith("execute_script")) {
-                boolean pass = true;
-                for (String script : new ArrayList<>(executedScripts))
-                    if (line.toString().contains(script)) {
-                        System.out.println("You cannot run a script that has been run before");
-                        pass = false;
-                        break;
-                    }
-                if (pass) {
-                    executedScripts.add(line.toString().replace("\r", "").replace("\n", ""));
-                    commands.add(line.toString().replace("\r", "").replace("\n", "")); // Windows adaptation
+
+        executeCommands(commands);
+    }
+
+    /**
+     * Parses the script file and populates the list of commands.
+     *
+     * @param reader   The InputStreamReader for reading the script file.
+     * @param commands The list to store the commands from the script.
+     * @throws IOException If there is an I/O error while reading the file.
+     */
+    private void parseScriptFile(InputStreamReader reader, List<String> commands) throws IOException {
+        int c;
+        StringBuilder line = new StringBuilder();
+
+        while ((c = reader.read()) != -1) {
+            if ((char) c == '\n') {
+                String finished = line.toString();
+                line = new StringBuilder();
+                if (finished.startsWith("execute_script")) {
+                    handleExecuteScriptCommand(finished, commands);
+                } else {
+                    commands.add(finished.replace("\r", "").replace("\n", ""));
                 }
-            } else
-                commands.add(line.toString().replace("\r", "").replace("\n", "")); // Windows adaptation
+            } else {
+                line.append((char) c);
+            }
         }
-        for (String command : commands)
+
+        handleRemainingLine(line.toString(), commands);
+    }
+
+    /**
+     * Handles an "execute_script" command line from the script file.
+     *
+     * @param command  The "execute_script" command line.
+     * @param commands The list to store the commands from the script.
+     */
+    private void handleExecuteScriptCommand(String command, List<String> commands) {
+        boolean pass = true;
+        for (String script : new ArrayList<>(executedScripts)) {
+            if (command.contains(script)) {
+                System.out.println("You cannot run a script that has been run before");
+                pass = false;
+                break;
+            }
+        }
+        if (pass) {
+            executedScripts.add(command.replace("\r", "").replace("\n", ""));
+            commands.add(command.replace("\r", "").replace("\n", ""));
+        }
+    }
+
+    /**
+     * Handles the remaining line in the script file.
+     *
+     * @param line     The remaining line.
+     * @param commands The list to store the commands from the script.
+     */
+    private void handleRemainingLine(String line, List<String> commands) {
+        if (line.length() > 0 && line.toString().replace("\r", "").replace("\n", "").length() > 0) {
+            if (line.startsWith("execute_script")) {
+                handleExecuteScriptCommand(line, commands);
+            } else {
+                commands.add(line.replace("\r", "").replace("\n", ""));
+            }
+        }
+    }
+
+    /**
+     * Executes a list of commands.
+     *
+     * @param commands The list of commands to execute.
+     */
+    private void executeCommands(List<String> commands) {
+        for (String command : commands) {
             Main.getCommandManager().executeInput(command);
+        }
     }
 
     @Override
